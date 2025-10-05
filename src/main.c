a9bec5bfd3adcb0566e74d0e00224b79d6813fff
Alba <quillhielo@gmail.com>
2025-10-05 20:53:01 +0200

    Implement print_stack function in main.c
    Added print_stack function to display stack values.

--- src/main.c
+++ src/main.c
@@ -12,19 +12,22 @@
 
 #include "push_swap.h"
 
-t_node	*node_init(int value)
+void	print_stack(t_node *s_a)
 {
-	t_node	*new;
-	new = (t_node *)malloc(sizeof(t_node *));
-	if (!new)
-		error("Allocating failure");
-	new->value = value;
-	new->prev = NULL;
-	new->next = NULL;
-	return (new);
+	t_node *num;
+	t_node	*temp;
+	num = s_a;
+	while (num)
+	{
+		printf("%i\n", num->value);
+		temp = num->next;
+		num = temp;
+	}
+
 }
 
 int	main(int argc, char **argv)
+
 {
 	int i;
 	t_framework *fw;
@@ -34,12 +37,26 @@ int	main(int argc, char **argv)
 	fw = ft_calloc(1, sizeof(t_framework *));
 	if (!fw)
 		error("Allocating failure");
-	
-	i = 0;
-	while(i < argc)
+	i = argc - 1;
+	while(i > 0)
 	{
 		parser(argv[i], fw);
-		i++;
+		i--;
 	}
+	print_stack(fw->stack_a);
+	printf("Número de dígitos: %i\n", fw->amount);
+	swap_a(fw);
+	print_stack(fw->stack_a);
+	push_b(fw);
+	printf("----------\n");
+	print_stack(fw->stack_a);
+	printf("----------\n");
+	print_stack(fw->stack_b);
+	push_a(fw);
+	printf("----------\n");
+	print_stack(fw->stack_a);
+	printf("----------\n");
+	print_stack(fw->stack_b);
 	return (0);
 }
+
