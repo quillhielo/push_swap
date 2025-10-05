726dfc3c8191159d1eeebf25491ebf6c7e450568
Alba <quillhielo@gmail.com>
2025-10-05 20:56:41 +0200

    Refactor push_swap.h: reorder and add functions

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -21,7 +21,6 @@ typedef struct s_node
 {
 	int				value;
 	struct s_node	*next;
-	struct s_node	*prev;
 }	t_node;
 
 typedef struct s_framework 
@@ -32,14 +31,19 @@ typedef struct s_framework
 }	t_framework;
 
 void	error(char *str);
-int		parser(char *str, t_framework *fw);
+t_node	*node_init(int value);
+void	place_node_a(t_framework *fw, t_node *new);
+void	place_node_b(t_framework *fw, t_node *new);
+int		matrix_len(char **split);
+void	free_matrix(char **matrix);
+void	dupe_check(int number, t_framework *fw);
 void	limits(int i);
 void	is_number(char *str);
-t_node	*node_init(int value);
-
-
-
-
-
+int		parser(char *str, t_framework *fw);
+void 	swap_a(t_framework *fw);
+void 	swap_b(t_framework *fw);
+void 	swap_ss(t_framework *fw);
+void    push_a(t_framework *fw);
+void    push_b(t_framework *fw);
 
 #endif
